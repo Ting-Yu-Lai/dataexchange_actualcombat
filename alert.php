2ccfba294777d96621f365988eaa19a8ecf90530
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-03 13:27:11 +0800

    完成alert

---
+++ alert.php
@@ -0,0 +1,48 @@
+<?php
+
+$data = [
+    [
+        'id' => 1,
+        'class' => 'alert-success',
+        'text' => '心 慢慢疼 慢慢冷'
+    ],
+    [
+        'id' => 2,
+        'class' => 'alert-info',
+        'text' => '慢慢等不到愛人.'
+    ],
+    [
+        'id' => 3,
+        'class' => 'alert-warning',
+        'text' => '付出一生 收回幾成.'
+    ],
+    [
+        'id' => 4,
+        'class' => 'alert-danger',
+        'text' => '情 不能分 不能恨.'
+    ],
+    [
+        'id' => 5,
+        'class' => 'alert-primary',
+        'text' => '不能太輕易信任.'
+    ],
+    [
+        'id' => 6,
+        'class' => 'alert-secondary',
+        'text' => '真愛一回 盡是傷痕.'
+    ],
+    [
+        'id' => 7,
+        'class' => 'alert-dark',
+        'text' => '慢慢 慢慢沒有感覺.'
+    ],
+    [
+        'id' => 8,
+        'class' => 'alert-light',
+        'text' => '慢慢 慢慢我被忽略.'
+    ],
+];
+
+// 僅輸出 JSON 數據
+echo json_encode($data);
+?>
